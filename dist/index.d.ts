04bb39955a2a8721433807f19f26ea492fb6def4
Camilo M <camilomantillaq13@gmail.com>
2025-06-07 18:13:14 -0500

    Build updated

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * isEven
+ * Determina si un número es par.
+ *
+ * @param times - number of iterations.
+ * @returns message.
+ */
+export declare function startTest(times: number): {
+    message: string;
+};
